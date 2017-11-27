232309e233d74a2454134798f5550e7ee983b37f
Ryan Spink <ryanspinktech@gmail.com>
2017-11-26 19:39:22 -0700

    fixed states_spec test
    added radar and twitter url to fabricated state, wasn't initially passing travisci

--- spec/requests/api/v1/states_spec.rb
+++ spec/requests/api/v1/states_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "states API" do
   it "returns each state" do
     VCR.use_cassette("states/each_state") do
-      Fabricate(:state, id:1, name:"Ohio", abbreviation: "OH", picture_url: "www.ohio.com")
-      Fabricate(:state, id:2, name:"Mississippi", abbreviation: "MS", picture_url: "www.mississippi.com")
-      Fabricate(:state, id:3, name:"Arizona", abbreviation: "AZ", picture_url: "www.arizona.com")
-      
+      Fabricate(:state, id:1, name:"Ohio", abbreviation: "OH", picture_url: "www.ohio.com", twitter_url: "https://twitter.com/ColoradoDOT?ref_src=twsrc%5Etfw" , radar_url:"https://radar.weather.gov/ridge/lite/N0R/FTG_loop.gif?854")
+      Fabricate(:state, id:2, name:"Mississippi", abbreviation: "MS", picture_url: "www.mississippi.com", twitter_url:"https://twitter.com/UDOTRegionTwo?ref_src=twsrc%5Etfw", radar_url: "https://radar.weather.gov/ridge/lite/N0R/MTX_loop.gif" )
+      Fabricate(:state, id:3, name:"Arizona", abbreviation: "AZ", picture_url: "www.arizona.com", twitter_url:"https://twitter.com/arizona", radar_url: "https://radar.weather.gov/arizona.gif" )
+
       get '/api/v1/states'
 
       expect(response).to be_success
@@ -41,31 +41,31 @@ describe "states API" do
       expect(state_three).to_not have_key  "altitude"
       expect(state_three).to_not have_key  "observance"
 
-      expect(state_one["id"]).to               eq(1)
-      expect(state_one["name"]).to             eq("Ohio")
-      expect(state_one["abbreviation"]).to     eq("OH")
+      expect(state_one["id"]).to              eq(1)
+      expect(state_one["name"]).to            eq("Ohio")
+      expect(state_one["abbreviation"]).to    eq("OH")
       expect(state_one["picture_url"]).to     eq("www.ohio.com")
-      expect(state_two["id"]).to               eq(2)
-      expect(state_two["name"]).to             eq("Mississippi")
-      expect(state_two["abbreviation"]).to     eq("MS")
+      expect(state_two["id"]).to              eq(2)
+      expect(state_two["name"]).to            eq("Mississippi")
+      expect(state_two["abbreviation"]).to    eq("MS")
       expect(state_two["picture_url"]).to     eq("www.mississippi.com")
-      expect(state_three["id"]).to             eq(3)
-      expect(state_three["name"]).to           eq("Arizona")
-      expect(state_three["abbreviation"]).to   eq("AZ")
-      expect(state_three["picture_url"]).to     eq("www.arizona.com")
+      expect(state_three["id"]).to            eq(3)
+      expect(state_three["name"]).to          eq("Arizona")
+      expect(state_three["abbreviation"]).to  eq("AZ")
+      expect(state_three["picture_url"]).to   eq("www.arizona.com")
 
-      expect(state_one["id"]).to_not               eq(2)
-      expect(state_one["name"]).to_not             eq("Mississippi")
-      expect(state_one["abbreviation"]).to_not     eq("AR")
+      expect(state_one["id"]).to_not              eq(2)
+      expect(state_one["name"]).to_not            eq("Mississippi")
+      expect(state_one["abbreviation"]).to_not    eq("AR")
       expect(state_one["picture_url"]).to_not     eq("www.google.com")
-      expect(state_two["id"]).to_not               eq(1)
-      expect(state_two["name"]).to_not             eq("Ohio")
-      expect(state_two["abbreviation"]).to_not     eq("AR")
+      expect(state_two["id"]).to_not              eq(1)
+      expect(state_two["name"]).to_not            eq("Ohio")
+      expect(state_two["abbreviation"]).to_not    eq("AR")
       expect(state_two["picture_url"]).to_not     eq("www.hamtime.com")
-      expect(state_three["id"]).to_not             eq(1)
-      expect(state_three["name"]).to_not           eq("Ohio")
-      expect(state_three["abbreviation"]).to_not   eq("AR")
-      expect(state_three["picture_url"]).to_not     eq("www.jabron.com")
+      expect(state_three["id"]).to_not            eq(1)
+      expect(state_three["name"]).to_not          eq("Ohio")
+      expect(state_three["abbreviation"]).to_not  eq("AR")
+      expect(state_three["picture_url"]).to_not   eq("www.jabron.com")
     end
   end
 end
